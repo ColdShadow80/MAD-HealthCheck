30144e70f0300a97fbd6fd9f13a3dcf24f9ac9ed
ColdShadow80 <46814978+ColdShadow80@users.noreply.github.com>
2022-10-09 14:06:12 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source ./config.ini
+
+ function my_used_disk_space()
+ {
+ 	du -sh ~ | cut -f1
+ }
+ 
+ function my_used_disk_space_percentage()
+ {
+  ##hardcoded path, to be reviewed later
+ 	df --output=pcent /dev/vda3 | tr -dc '0-9'
+ }
